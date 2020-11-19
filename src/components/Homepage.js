44907070638d256d0320cc900ea785dbfb5ea8a7
hishuverma <74209366+hishuverma@users.noreply.github.com>
2020-11-19 22:00:25 +0530

    Update Homepage.js

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -52,8 +52,9 @@ export default class Homepage extends Component{
 <React.Fragment>
 <div className="banner-video">
 	<div className="video-container"> 
-	    <video loop="" autoPlay="" playsInline="" src="https://player.vimeo.com/external/353890750.hd.mp4?s=95cdb39d0c577123df3a219a756d7d6c6550eee9&amp;profile_id=175&amp;title=0&amp;byline=0&amp;portrait=0&amp;color=000000&amp;muted=1&amp;autoplay=1&amp;autopause=0&amp;playsinline=1&amp;loop=1&amp;background=1"></video>
-		
+	   <video width="100%" height="760" loop autoPlay playsInline>
+ 	     <source src="https://player.vimeo.com/external/353890750.hd.mp4" type="video/mp4">
+ 	   </video>
 	</div>
 	<div className="bnrscearch"> 
 		<h1 className="heading1">Discover Trusted Marijuana Businesse in<br/>Your Communinty</h1>
