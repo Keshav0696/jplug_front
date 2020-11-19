a537c407ed2ab2927138e08b2d38da7d30ad55e2
hishuverma <74209366+hishuverma@users.noreply.github.com>
2020-11-19 22:02:48 +0530

    Update Homepage.js

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -51,8 +51,8 @@ export default class Homepage extends Component{
 
 <React.Fragment>
 <div className="banner-video">
-	<div className="video-container"> 
-	   <video width="100%" height="760" loop autoPlay playsInline>
+	    <div className="video-container"> 
+	   <video width="100%" height="760" loop autoPlay controls playsInline>
  	     <source src="https://player.vimeo.com/external/353890750.hd.mp4" type="video/mp4">
  	   </video>
 	</div>
