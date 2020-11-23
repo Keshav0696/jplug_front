e7ceadec4b75aa4e6e0e9af5980dc89e61e5b7df
hishuverma <74209366+hishuverma@users.noreply.github.com>
2020-11-24 00:00:19 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -22,15 +22,7 @@ export default class Footer extends Component{
 					<li><a href="#"><img src="images/footer/ft5.png" /></a></li>
 				</ul>
 			</div>
-	      		<ul className="footer-socil2">
-			  <li><a href="#"><i className="fa fa-facebook"></i></a></li>
-			  <li><a href="#"><i className="fa fa-twitter"></i></a></li>
-			  <li><a href="#"><i className="fa fa-linkedin"></i></a></li>
-			  <li><a href="#"><i className="fa fa-pinterest-p"></i></a></li>
-			  <li><a href="#"><i className="fa fa-reddit-alien"></i></a></li>
-			  <li><a href="#"><i className="fa fa-maxcdn"></i></a></li>
-			  <li><a href="#"><i className="fa fa-whatsapp"></i></a></li>
-			</ul>
+	      		<div class="footer-new"><ul class="footelink"><li>COMPANY</li><li><a href="#">Contact Us</a></li><li><a href="#">Blog</a></li><li><a href="#">Career</a></li></ul><ul class="footelink"><li>USERS</li><li><a href="#">Login</a></li><li><a href="#">Register</a></li><li><a href="#">Forget Password?</a></li></ul><p class="text-center">JointPlug Pvt.Ltd. © 2012-2020</p></div>
 			<div className="col-md-12">
 				<ul className="footelink">
 					<li>COMPANY</li>
