eb5a39d0eca6574484e484176aa5e33404f0bc67
hishuverma <74209366+hishuverma@users.noreply.github.com>
2020-11-25 20:49:46 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -22,8 +22,58 @@ export default class Footer extends Component{
 					<li><a href="#"><img src="images/footer/ft5.png" /></a></li>
 				</ul>
 			</div>
-	      		<div class="footer-new"><ul class="footelink"><li>COMPANY</li><li><a href="#">Contact Us</a></li><li><a href="#">Blog</a></li><li><a href="#">Career</a></li></ul><ul class="footelink"><li>USERS</li><li><a href="#">Login</a></li><li><a href="#">Register</a></li><li><a href="#">Forget Password?</a></li></ul><p class="text-center">JointPlug Pvt.Ltd. © 2012-2020</p></div>
-			<div className="col-md-12">
+	      		<div class="footer-new">
+	      			<div className="suggested-text-home-page">
+        <p>Suggested in Washington</p>
+    </div>
+    
+    <div className="suggested-iteams-container">
+    
+      <div className="suggested-iteams">
+		  <a href="/#">
+        <img src="images/11-n.png" />
+          <label>Strains</label>
+		  </a>
+    </div>
+        
+             <div className="suggested-iteams">
+		<a href="/#">
+        <img src="images/22-n.png" />
+          <label>Deals</label>
+		  </a>
+    </div>
+        
+             <div className="suggested-iteams">
+			 <a href="/#">
+        <img src="images/33-n.png" />
+          <label>Recreational</label>
+		  </a>
+    </div>
+        
+             <div className="suggested-iteams">
+			 <a href="/#">
+        <img src="images/44-n.png" />
+          <label>Dispensaries</label>
+		  </a>
+    </div>
+        
+             <div className="suggested-iteams">
+				 <a href="/#">
+        <img src="images/55-n.png" />
+          <label>Deliveries</label>
+		  </a>
+    </div>
+        
+             <div className="suggested-iteams">
+				 <a href="/#">
+        <img src="images/66-n.png" />
+          <label>Doctors</label>
+		  </a>
+    </div>
+           
+    </div>
+	      		</div>
+	      	<div className="col-md-12">
 				<ul className="footelink">
 					<li>COMPANY</li>
 					<li><a href="#">Contact Us</a></li>
