8d47ff11c480ca45e82a5bc1b086df9e8d2a9445
Ravi <raviarya2689@gmail.com>
2020-12-25 01:07:48 +0530

    tour route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter,Route,  Switch} from 'react-router-dom';
 import Header from './components/Header';
 import Homepage from './components/Homepage';
+import Tourpage from './components/Tourpage';
 
 export default class App extends Component {
     render() {
@@ -10,7 +11,8 @@ export default class App extends Component {
                 <div>
                     <Header />
                     <Switch>
-                    <Route exact path='/' component={Homepage} />
+                        <Route exact path='/tour' component={Tourpage} />
+                        <Route exact path='/' component={Homepage} />
                     </Switch>
                 </div>
             </BrowserRouter>
