bad1c65005470ae7a3eed50c6d04abb791ca7710
unknown <raviarya2689@gmail.com>
2020-12-25 20:57:46 +0530

    login css

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,13 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './login.css';
+import Footer from '../Footer';
+import {Link, useHistory} from 'react-router-dom';
 
-class Login extends Component {
-
-    render() {
+function Login (props) {
+  const history = useHistory()
         return (
             <React.Fragment>
-                <div className="container">
-                  <form action="/action_page.php">
+                <div className="login_container">
+                  <form>
                     <div className="row">
                       <h2 style={{textAlign:"center"}}>Login with Social Media or Manually</h2>
                       <div className="vl">
@@ -38,19 +39,19 @@ class Login extends Component {
                     </div>
                       <div className="bottom-container">
                         <div className="row">
-                          <div className="col">
-                            <a href="#" style={{color:"white"}} className="btn">Sign up</a>
+                          <div className="col m-2">
+                            <a href="#" style={{color:"white", background: '#dd4b39'}} className="btn" onClick={() => {history.push('/signup')}}>Sign up</a>
                           </div>
-                          <div className="col">
-                            <a href="#" style={{color:"white"}} className="btn">Forgot password?</a>
+                          <div className="col m-2">
+                            <a href="#" style={{color:"white", background: '#dd4b39'}} className="btn" onClick={() => {history.push('/')}}>Forgot password?</a>
                           </div>
                         </div>
                       </div>
                   </form>
                 </div>
+                <Footer />
             </React.Fragment>
         );
-    }
 }
 
 export default Login;
